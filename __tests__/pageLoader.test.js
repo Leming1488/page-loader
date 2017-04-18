9af8a6030a10720e7d05a806abfc191d0eb25cf2
Козельский Павел Сергеевич <pkozelskiy@n-core.ru>
2017-04-18 11:51:35 +0300

    [point-1] add test

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,10 +1,16 @@
 // @flow
 
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
 import nock from 'nock';
 import pageLoader from '../src';
 
-const url = 'https://hexlet.io/courses';
+const host = 'https://localhost';
+
+axios.defaults.host = host;
+axios.defaults.adapter = httpAdapter;
+
 test('Download page from url to a current directory', () => {
-  pageLoader('https://api.github.com/users/codeheaven-io', __dirname)
-    .then(state => expect(state).toBe('true'));
+  return pageLoader('https://api.github.com/users/codeheaven-io', __dirname)
+    .then(state => expect(state).toBe(true));
 });
--- src/index.js
+++ src/index.js
@@ -2,22 +2,14 @@
 import urlApi from 'url';
 import fs from 'mz/fs';
 import axios from 'axios';
-// fs.write(`${parsedUrl.hostname}${parsedUrl.pathname}.html`, res)
-export default (url, path) => {
+
+export default (url, path = __dirname) => {
   const parsedUrl = urlApi.parse(url);
-  fs.stat(path)
-    .then((stats) => {
-      if (stats.isDirectory) {
-        axios.get('https://travelpayoutsjs.herokuapp.com')
-          .then(res => console.log(res))
-          .catch(error => {
-            console.log(error);
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          });
-      }
-    })
+  const fileName = `${parsedUrl.hostname}${parsedUrl.pathname}`.replace(/[^A-Za-z0-9]/g, '-');
+  return fs.stat(path)
+    .then(stats => (stats.isDirectory ? axios.get(url) : console.log('is not directory')))
+    .then(res => fs.writeFile(`${path}/${fileName}.html`, `${res.data}`, 'utf8'))
+    .then(() => Promise.resolve(true))
     .catch(e => console.log(e));
 };
 
