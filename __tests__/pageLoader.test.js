6c004347e918e23f2cf36bfed3bd23dfe1ea4827
Козельский Павел Сергеевич <pkozelskiy@n-core.ru>
2017-04-19 13:49:25 +0300

    [point-2] fix async test

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,17 +1,15 @@
 // @flow
-import axios from 'axios';
-import httpAdapter from 'axios/lib/adapters/http';
 import fs from 'mz/fs';
 import os from 'os';
 import nock from 'nock';
 import path from 'path';
 import rimraf from 'rimraf';
+import axiosHttpAdapter from '../src/lib/aixosHttpAdapter';
 import pageLoader from '../src';
 
 const host = 'https://localhost';
+axiosHttpAdapter(host);
 const address = `${host}${path.sep}index`;
-const tmpDir = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
-const page = fs.readFileSync(path.resolve('./__tests__/__fixtures__/', 'test-page.html'));
 const defaultDir = path.resolve('./localhost-index.html');
 
 const testPage =
@@ -20,17 +18,33 @@ const testPage =
   <head>
     <meta charset="utf-8">
     <title>Test page</title>
+    <script src="/assets/js/index.js"></script>
+    <script src="/assets/js/vendor/jquery.js"></script>
+    <link href="/assets/css/index.css" rel="stylesheet"/>
   </head>
   <body>
-    <h1>Test</h1>
+    <h1 class="text-center">Download</h1>
+    <img alt="img1" src="/assets/img/logo-background-1.jpg"/>
+    <img alt="img2" src="/assets/img/logo-background-2.jpg"/>
+    <img alt="img3" src="/assets/img/logo-background-3.jpg"/>
+    <a  href="#"><img alt="photo" src="/assets/img/icon-photo.png"/></a>
+    <p>You are probably looking for either jsdom , jquery or cheerio. <img alt="warning" src="/assets/img/icon-warning.png"/>  What you are doing is called screen scraping, extracting data from a site. jsdom/jquery offer complete set of tools but cheerio is much faster </p>
   </body>
 </html>
 `;
 
-axios.defaults.host = host;
-axios.defaults.adapter = httpAdapter;
 
 describe('page-loader', () => {
+  const tmpDir = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+  const page = fs.readFileSync(path.resolve('./__tests__/__fixtures__/', 'test-page.html'));
+
+  const assetsPath = './__tests__/__fixtures__/assets/';
+  const ner = fs.readdir(assetsPath)
+    .then(nodes => Promise.all(nodes.reduce((acc, dir) => [...acc, fs.readdir(assetsPath + dir)], [])))
+    .then((list) => {
+      console.log(list.join(','));
+    });
+
   beforeEach(() => {
     nock(host)
       .get('/index')
@@ -42,16 +56,47 @@ describe('page-loader', () => {
     rimraf(defaultDir, () => console.log('clear defaultDir'));
   });
 
-  test('Download page from url to a current directory', () => (
-    Promise.resolve(pageLoader(address, tmpDir))
-    .then(() => fs.readFile(`${tmpDir}${path.sep}localhost-index.html`, 'utf8'))
-    .then(date => expect(date).toBe(testPage))
-  ));
+  test('Download page from url to a current directory', (done) => {
+    function callback() {
+      fs.readFile(`${tmpDir}${path.sep}localhost-index.html`, 'utf8')
+      .then((data) => {
+        expect(data).toBe(testPage);
+        done();
+      })
+      .catch((e) => {
+        done.fail(e);
+      });
+    }
+    pageLoader(address, tmpDir, callback);
+  });
 
-  test('Download page from url to a directory by default', () => (
-    Promise.resolve(pageLoader(address))
-    .then(() => fs.readFile(defaultDir, 'utf8'))
-    .then(date => expect(date).toBe(testPage))
-  ));
+  test('Download page from url to a directory by default', (done) => {
+    function callback() {
+      fs.readFile(defaultDir, 'utf8')
+      .then((data) => {
+        expect(data).toBe(testPage);
+        done();
+      })
+      .catch((e) => {
+        done.fail(e);
+      });
+    }
+    pageLoader(address, undefined, callback);
+  });
+
+  test('Download assets', (done) => {
+    function callback() {
+      fs.readdir(assetsPath)
+      .then(nodes => Promise.all(nodes.reduce((acc, dir) => [...acc, fs.readdir(`${assetsPath}${dir}`)], [])))
+      .then((list) => {
+        expect(list.join(',')).toBe(' ');
+        done();
+      })
+      .catch((e) => {
+        done.fail(e);
+      });
+    }
+    pageLoader(address, tmpDir, callback);
+  });
 });
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import fs from 'mz/fs';
 import axios from 'axios';
 import path from 'path';
 
-export default (url, directory = './') => {
+export default (url, directory = './', callback) => {
   const parsedUrl = urlApi.parse(url);
   const fileName = `${parsedUrl.hostname}${parsedUrl.pathname}`.replace(/[^A-Za-z0-9]/g, '-');
   const filePath = path.format({
@@ -12,10 +12,13 @@ export default (url, directory = './') => {
     name: fileName,
     ext: '.html',
   });
-  return fs.stat(directory)
+  fs.stat(directory)
   .then(stats => (stats.isDirectory ? axios.get(url) : new Error('Directory does not exist')))
-  .then(res => fs.writeFile(filePath, `${res.data}`, 'utf8'))
-  .then(() => console.log('succesfully written'))
-  .catch(e => console.log(e));
+    .then(res => {
+      // console.log(res.data);
+      return fs.writeFile(filePath, res.data, 'utf8');
+    })
+    .then(() => callback('succesfully written'))
+  .catch(e => e);
 };
 
